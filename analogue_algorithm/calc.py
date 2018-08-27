Changes verification to consider entire grid to remove bug in best member calculation

analogue_algorithm/calc.py
@@ -81,6 +81,56 @@ def find_analogue(forecast_date, *args):
     return an_idx
 
 
+# def verify_members(dataset, observations, parameters, mem_list):
+#     """
+#     Calculates the sum of RMSE for each dataset member over the specified time range
+#
+#     :param dataset: xarray dataset. Input forecast dataset
+#     :param observations: xarray dataset. Observations dataset
+#     :param parameters: dict. Dictionary of parameter values as below
+#               forecast_hour: float. Valid forecast hour of forecast dataset
+#               threshold: float. Threshold for verification masking
+#               sigma: float. Standard deviation of guassian filter
+#               start_date: str. Start date of verification period
+#               end_date: str. End date of verification period
+#     :param mem_list: List of string member names
+#     :return: Sum of RMSE for each member over verification period
+#     """
+#     tot_rmse = {}
+#     for mem in mem_list:
+#         tot_rmse[mem] = 0.
+#
+#     dates = pd.date_range(start=parameters['start_date'],
+#                           end=parameters['end_date'],
+#                           freq=parameters['dt'])
+#     for date in dates:
+#         obs_date = date + pd.Timedelta(hours=parameters['forecast_hour'])
+#         obs_data = observations.sel(time=obs_date)
+#         obs_smooth = gaussian_filter(obs_data, parameters['sigma'])
+#         for mem in mem_list:
+#             mem_rmse = tot_rmse[mem]
+#             mem_data = dataset[mem].sel(
+#                                         time=date
+#                                         ).where(obs_smooth >= parameters['threshold'])
+#             rmse_from_obs = rmse(mem_data.values,
+#                                  obs_data.where(
+#                                                 obs_smooth >= parameters['threshold']
+#                                                 ).values)
+#             fcst_smooth = gaussian_filter(dataset[mem].sel(time=date),
+#                                           parameters['sigma'])
+#             mem_data = dataset[mem].sel(
+#                 time=date
+#                 ).where(fcst_smooth >= parameters['threshold'])
+#             rmse_from_fcst = rmse(mem_data.values, obs_data.where(
+#                                     fcst_smooth >= parameters['threshold']).values)
+#             if np.isnan(rmse_from_obs):
+#                 rmse_from_obs = 0.
+#             if np.isnan(rmse_from_fcst):
+#                 rmse_from_fcst = 0.
+#             error = rmse_from_fcst + rmse_from_obs
+#             tot_rmse[mem] = mem_rmse + error
+#     return tot_rmse
+
 def verify_members(dataset, observations, parameters, mem_list):
     """
     Calculates the sum of RMSE for each dataset member over the specified time range
@@ -106,24 +156,13 @@ def verify_members(dataset, observations, parameters, mem_list):
     for date in dates:
         obs_date = date + pd.Timedelta(hours=parameters['forecast_hour'])
         obs_data = observations.sel(time=obs_date)
-        obs_smooth = gaussian_filter(obs_data, parameters['sigma'])
         for mem in mem_list:
             mem_rmse = tot_rmse[mem]
-            mem_data = dataset[mem].sel(time=date).where(obs_smooth >= parameters['threshold'])
-            rmse_from_obs = rmse(mem_data.values,
-                                 obs_data.where(obs_smooth >= parameters['threshold']).values)
-            fcst_smooth = gaussian_filter(dataset[mem].sel(time=date),
-                                          parameters['sigma'])
-            mem_data = dataset[mem].sel(
-                time=date
-                ).where(fcst_smooth >= parameters['threshold'])
-            rmse_from_fcst = rmse(mem_data.values, obs_data.where(
-                                    fcst_smooth >= parameters['threshold']).values)
-            mean_rmse = (rmse_from_fcst + rmse_from_obs) / 2.
-            if np.isnan(mean_rmse):
-                tot_rmse[mem] = mem_rmse
-            else:
-                tot_rmse[mem] = mem_rmse + mean_rmse
+            mem_data = dataset[mem].sel(time=date)
+            error = rmse(mem_data.values, obs_data.values)
+            if np.isnan(error):
+                error = 0.
+            tot_rmse[mem] = mem_rmse + error
     return tot_rmse
 
 

analogue_algorithm/tests/test_calc.py
@@ -66,21 +66,25 @@ def test_verif_members():
         'forecast_hour': 0,
         'threshold': 10,
         'sigma': 2,
-        'start_date': '2015-12-28T12:00:00',
+        'start_date': '2015-12-27T12:00:00',
         'end_date': '2015-12-29T12:00:00',
         'dt': '1D'
         }
     date_array = pd.date_range(start=param['start_date'],
                                end=param['end_date'],
                                freq=param['dt'])
-    data = np.ones((2, 5, 5)) * np.linspace(5, 30, 50).reshape(2, 5, 5)
+    data = np.ones((3, 5, 5)) * np.linspace(0, 30, 75).reshape(3, 5, 5)
+    data[1, ] = 0.
+    mem1_data = data * 10.
+    mem3_data = data * 5.
     lat = np.linspace(40, 45, 5)
     lon = np.linspace(-105, -100, 5)
     mlon, mlat = np.meshgrid(lon, lat)
-    mem_list = ['mem1', 'mem2']
-    dataset = xr.Dataset({'mem1': (['time', 'latitude', 'longitude'], data * 2.),
+    mem_list = ['mem1', 'mem2', 'mem3', 'mem4']
+    dataset = xr.Dataset({'mem1': (['time', 'latitude', 'longitude'], mem1_data),
                           'mem2': (['time', 'latitude', 'longitude'], data),
-                          'mem3': (['time', 'latitude', 'longitude'], data * 1.5),
+                          'mem3': (['time', 'latitude', 'longitude'], mem3_data),
+                          'mem4': (['time', 'latitude', 'longitude'], data * 15.),
                           'mean': (['time', 'latitude', 'longitude'], data * 0.5)},
                          coords={'time': date_array,
                                  'lat': (['latitude', 'longitude'], mlat),
@@ -91,7 +95,7 @@ def test_verif_members():
                              'lon': (['latitude', 'longitude'], mlon)})
     tot_rmse = verify_members(dataset, obs.total_precipitation, param, mem_list)
     members = np.array([tot_rmse[mem] for mem in mem_list])
-    best_mem = members.argmin()
+    best_mem = np.nanargmin(members)
     assert_almost_equal(best_mem, 1, 4)
 
 

calc_analogue.py
@@ -60,8 +60,7 @@ param = {
     'an_start_date': '2015-10-01T12:00:00',
     'an_end_date': '2015-12-29T12:00:00',
     'dt': '1D',
-    'comments': 'Calculate analogues for outer domain, now includes all PBL members (YSU'
-                'previously excluded)'
+    'comments': 'Calculate analogues for outer domain after fixing verification error'
     }
 
 verif_param = {
@@ -477,9 +476,10 @@ plt.savefig(param['directory']+param['method']+'_d0'+str(int(dom))+'_ranks.png')
 # Find average best member
 tot_rmse = verify_members(pcp, stage4.total_precipitation, verif_param, mem_list)
 mean_mp = np.array([tot_rmse[mem] for mem in mp_list])
-mean_best_mp = mp_list[mean_mp.argmin()]
+print(tot_rmse, flush=True)
+mean_best_mp = mp_list[np.nanargmin(mean_mp)]
 mean_pbl = np.array([tot_rmse[mem] for mem in pbl_list])
-mean_best_pbl = pbl_list[mean_pbl.argmin()]
+mean_best_pbl = pbl_list[np.nanargmin(mean_pbl)]
 
 # Compare analogue physics with average best physics
 print('Generating Timeseries', flush=True)